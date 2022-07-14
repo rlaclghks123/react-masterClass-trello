2e099c4bfc414aa4adb6dd8a8fb9eaf4d06f7525
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-14 18:19:16 +0900

    typescript적용 및 css작업 및 placeholder 사용
    1. typescript 적용
    2. Wrapper, Boards,Board,Card css작업
    3. placeholder사용 drop 되는 부분의 크기가 고정

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+const toDos = ["1", "2", "3", "4", "5"];
+function App() {
+  const onDragEnd = () => { };
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((todo, index) => <Draggable draggableId={todo} index={index}>
+                  {(magic) => (
+                    <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                      {todo}
+                    </Card>
+                  )}
+                </Draggable>)}
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
+  );
+}
+export default App;
