3e27ce94a00e8581b675ecb5394b33689cfb5db3
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-25 17:52:17 +0900

    localStorage 추가

--- src/atoms.ts
+++ src/atoms.ts
@@ -8,6 +8,22 @@ interface IToDoState {
   [key: string]: IToDo[];
 }
 
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem("toDo");
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: string) => {
+      const confirm = newValue.length === 0;
+      confirm
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const toDoState = atom<IToDoState>({
   key: "toDo",
   default: {
@@ -15,4 +31,5 @@ export const toDoState = atom<IToDoState>({
     DOING: [],
     DONE: [],
   },
+  effects: [localStorageEffect("toDo")],
 });
