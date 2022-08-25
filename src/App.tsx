0b99c1409cbc8288acc120a2ea2d9a63d5a643a0
chiman <rlaclghks123@naver.com>
2022-08-25 20:13:30 +0900

    Trello component 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,10 @@
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
-import styled from "styled-components";
-import { toDoState } from "./atoms";
-import Board from "./Components/Board";
-import TrashCan from "./Components/TrashCan";
-
-const Wrapper = styled.div`
-  display: flex;
-  max-width: 500px;
-  width: 100%;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const Boards = styled.div`
-  display: grid;
-  width: 100%;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 15px;
-`;
+import Trello from "./Components/Trello";
 
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-
-  const onDragEnd = (info: DropResult) => {
-    const { draggableId, destination, source } = info;
-    if (!destination) return;
-    if (source.droppableId === destination?.droppableId) {
-      setToDos(allToDos => {
-        const copyBoard = [...allToDos[source.droppableId]];
-        copyBoard.splice(source.index, 1);
-        copyBoard.splice(destination?.index, 0, draggableId);
-        return { ...allToDos, [source.droppableId]: copyBoard };
-      });
-    } else if (destination.droppableId === "trashcan") {
-      setToDos(allBoards => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        boardCopy.splice(source.index, 1);
-        return { ...allBoards, [source.droppableId]: boardCopy };
-      });
-    } else if (source.droppableId !== destination?.droppableId) {
-      setToDos(allToDos => {
-        const sourceBoard = [...allToDos[source.droppableId]];
-        const destinationBoard = [...allToDos[destination.droppableId]];
-
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, draggableId);
-
-        return {
-          ...allToDos,
-          [source.droppableId]: sourceBoard,
-          [destination?.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map(boardId => (
-            <Board key={boardId} boardId={boardId} toDos={toDos[boardId]} />
-          ))}
-        </Boards>
-        <TrashCan />
-      </Wrapper>
-    </DragDropContext>
+    <>
+      <Trello />
+    </>
   );
 }
 export default App;
